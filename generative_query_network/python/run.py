5d5a7a9697a91c17a0ebc09758bcf41adef2e742
musyoku <musyoku@users.noreply.github.com>
2018-06-16 20:48:53 +0900

    add files

--- generative_query_network/python/run.py
+++ generative_query_network/python/run.py
@@ -18,12 +18,25 @@ def create_scene():
     scene = gqn.three.Scene()
     objects = []
     room = create_cornell_box()
-    scene.add(room, position=(0, 1, 0))
+    # scene.add(room, position=(0, 1, 0))
+
+    wall = create_object("cube", scale=(7.0, 3.0, 1.0))
+    scene.add(wall, position=(0.0, 0.0, 3.0))
+
+    # wall = create_object("cube", scale=(7.0, 3.0, 1.0))
+    # scene.add(wall, position=(0.0, 0.0, -3.0))
+
+    # wall = create_object("cube", scale=(7.0, 3.0, 1.0))
+    # scene.add(wall, position=(3.0, 0.0, 0.0), rotation=(0, math.pi / 2.0, 0.0))
+
+    # wall = create_object("cube", scale=(7.0, 3.0, 1.0))
+    # scene.add(wall, position=(0.0, 0.0, 3.0), rotation=(0, math.pi / 2.0, 0.0))
+
     obj = create_object("cube", scale=(0.5, 0.5, 0.5))
-    # scene.add(obj, position=(0.0, 0.0, 1.0))
+    scene.add(obj, position=(0.5, 0.0, 0.5))
     objects.append(obj)
     obj = create_object("polyhedron", scale=(0.5, 0.5, 0.5))
-    # scene.add(obj, position=(-0.1, 0.0, -0.1))
+    scene.add(obj, position=(-0.5, 0.0, -0.5))
     objects.append(obj)
     return scene, room, objects
 
@@ -32,12 +45,12 @@ def main():
     screen_size = (64, 64)
     scene, room, objects = create_scene()
     camera = gqn.three.PerspectiveCamera(
-        eye=(0.0, 0.25, 1.0),
+        eye=(1.0 * math.cos(math.pi * 6.889512689322029), 2.5, 1.0 * math.sin(math.pi * 6.889512689322029)),
         center=(0, 0, 0),
         up=(0, 1, 0),
         fov_rad=math.pi / 2.0,
         aspect_ratio=screen_size[0] / screen_size[1],
-        z_near=0.1,
+        z_near=0.01,
         z_far=100)
 
     figure = gqn.viewer.Figure()
@@ -52,17 +65,17 @@ def main():
         depth_map = np.full(screen_size, 1.0, dtype="float32")
         face_index_map = np.zeros(screen_size, dtype="int32")
         object_index_map = np.zeros(screen_size, dtype="int32")
-        # camera.look_at(
-        #     eye=(1.0 * math.cos(rad), 0.5, 1.0 * math.sin(rad)),
-        #     center=(0.0, 0.0, 0.0),
-        #     up=(0.0, 1.0, 0.0),
-        # )
+        camera.look_at(
+            eye=(1.0 * math.cos(rad), 2.5, 1.0 * math.sin(rad)),
+            center=(0.0, 0.0, 0.0),
+            up=(0.0, 1.0, 0.0),
+        )
+        # print(rad)
         gqn.renderer.render_depth_map(scene, camera, face_index_map,
                                       object_index_map, depth_map)
-
+        
         axis_depth_map.update(
-            np.uint8((1.0 - depth_map) / (1.0 - np.amin(depth_map)) * 255))
-        return
+            np.uint8(np.clip((1.0 - depth_map), 0.0, 1.0) * 255))
         if window.closed():
             return
 
--- three/core/renderer/depth_map.cpp
+++ three/core/renderer/depth_map.cpp
@@ -31,7 +31,6 @@ namespace renderer {
                 update_depth_map(object_index, object.get(), pvm_mat, np_face_index_map, np_depth_map);
             }
         }
-
         // 各画素ごとに最前面を特定する
         void update_depth_map(
             int object_index,
@@ -47,15 +46,20 @@ namespace renderer {
             int map_height = np_depth_map.shape(0);
             int map_width = np_depth_map.shape(1);
 
+            // std::cout << map_width << "x" << map_height << std::endl;
+
             auto depth_map = np_depth_map.mutable_unchecked<2>();
             auto object_index_map = np_face_index_map.mutable_unchecked<2>();
             auto face_index_map = np_face_index_map.mutable_unchecked<2>();
 
-            auto to_projected_coordinate = [](int p, int size) -> float {
-                return 2.0 * (p / (float)(size - 1) - 0.5);
+            auto to_projected_coordinate = [](int p, int size) -> double {
+                return 2.0 * ((2.0 * p + 1.0) / (double)(2.0 * size) - 0.5);
             };
 
             for (int face_index = 0; face_index < num_faces; face_index++) {
+                if (face_index != 2) {
+                    // continue;
+                }
                 glm::vec3i& face = faces[face_index];
                 glm::vec4f vfa = pvm_mat * vertices[face[0]];
                 glm::vec4f vfb = pvm_mat * vertices[face[1]];
@@ -66,50 +70,72 @@ namespace renderer {
                 vfb /= glm::abs(vfb.w);
                 vfc /= glm::abs(vfc.w);
 
-                std::cout << "a: " << vfa.x << ", " << vfa.y << ", " << vfa.z << std::endl;
-                std::cout << "b: " << vfb.x << ", " << vfb.y << ", " << vfb.z << std::endl;
-                std::cout << "c: " << vfc.x << ", " << vfc.y << ", " << vfc.z << std::endl;
+                // std::cout << "face: " << face_index << std::endl;
+                // std::cout << "a: " << vfa.x << ", " << vfa.y << ", " << vfa.z << std::endl;
+                // std::cout << "b: " << vfb.x << ", " << vfb.y << ", " << vfb.z << std::endl;
+                // std::cout << "c: " << vfc.x << ", " << vfc.y << ", " << vfc.z << std::endl;
 
                 // カリングによる裏面のスキップ
                 // 面の頂点の並び（1 -> 2 -> 3）が時計回りの場合描画しない
                 if ((vfa.y - vfc.y) * (vfa.x - vfb.x) < (vfa.y - vfb.y) * (vfa.x - vfc.x)) {
+                    // std::cout << "skipped" << std::endl;
                     continue;
                 }
 
                 // 全画素についてループ
                 for (int yi = 0; yi < map_height; yi++) {
                     // yi \in [0, map_height] -> yf \in [-1, 1]
-                    float yf = -to_projected_coordinate(yi, map_height);
+                    double yf = -to_projected_coordinate(yi, map_height);
+                    // std::cout << "yi = " << yi << ", yf = " << yf << std::endl;
                     // y座標が面の外部ならスキップ
                     if ((yf > vfa.y && yf > vfb.y && yf > vfc.y) || (yf < vfa.y && yf < vfb.y && yf < vfc.y)) {
+                        // std::cout << "outside y: " << yi << " : " << yf << std::endl;
                         continue;
                     }
                     for (int xi = 0; xi < map_width; xi++) {
                         // xi \in [0, map_width] -> xf \in [-1, 1]
-                        float xf = to_projected_coordinate(xi, map_width);
+                        double xf = to_projected_coordinate(xi, map_width);
+                        // std::cout << "xi = " << xi << ", xf = " << xf << std::endl;
 
                         // xyが面の外部ならスキップ
                         // Edge Functionで3辺のいずれかの右側にあればスキップ
                         // https://www.cs.drexel.edu/~david/Classes/Papers/comp175-06-pineda.pdf
                         if ((yf - vfa.y) * (vfb.x - vfa.x) < (xf - vfa.x) * (vfb.y - vfa.y) || (yf - vfb.y) * (vfc.x - vfb.x) < (xf - vfb.x) * (vfc.y - vfb.y) || (yf - vfc.y) * (vfa.x - vfc.x) < (xf - vfc.x) * (vfa.y - vfc.y)) {
+                            // std::cout << "outside face: " << xi << ", " << yi << " : " << xf << ", " << yf << std::endl;
                             continue;
                         }
 
                         // 重心座標系の各係数を計算
                         // http://zellij.hatenablog.com/entry/20131207/p1
-                        float lambda_1 = ((vfb.y - vfc.y) * (xf - vfc.x) + (vfc.x - vfb.x) * (yf - vfc.y)) / ((vfb.y - vfc.y) * (vfa.x - vfc.x) + (vfc.x - vfb.x) * (vfa.y - vfc.y));
-                        float lambda_2 = ((vfc.y - vfa.y) * (xf - vfc.x) + (vfa.x - vfc.x) * (yf - vfc.y)) / ((vfb.y - vfc.y) * (vfa.x - vfc.x) + (vfc.x - vfb.x) * (vfa.y - vfc.y));
-                        float lambda_3 = 1.0 - lambda_1 - lambda_2;
+                        double lambda_1 = ((vfb.y - vfc.y) * (xf - vfc.x) + (vfc.x - vfb.x) * (yf - vfc.y)) / ((vfb.y - vfc.y) * (vfa.x - vfc.x) + (vfc.x - vfb.x) * (vfa.y - vfc.y));
+                        double lambda_2 = ((vfc.y - vfa.y) * (xf - vfc.x) + (vfa.x - vfc.x) * (yf - vfc.y)) / ((vfb.y - vfc.y) * (vfa.x - vfc.x) + (vfc.x - vfb.x) * (vfa.y - vfc.y));
+                        double lambda_3 = 1.0 - lambda_1 - lambda_2;
 
                         // 面f_nのxy座標に対応する点のz座標を求める
                         // https://www.scratchapixel.com/lessons/3d-basic-rendering/rasterization-practical-implementation/visibility-problem-depth-buffer-depth-interpolation
-                        float z_face = 1.0 / (lambda_1 / vfa.z + lambda_2 / vfb.z + lambda_3 / vfc.z);
+                        // double z_face = 1.0 / (lambda_1 / vfa.z + lambda_2 / vfb.z + lambda_3 / vfc.z);
+                        double z_face = lambda_1 * vfa.z + lambda_2 * vfb.z + lambda_3 * vfc.z;
+                        double x_face = lambda_1 * vfa.x + lambda_2 * vfb.x + lambda_3 * vfc.x;
+                        double y_face = lambda_1 * vfa.y + lambda_2 * vfb.y + lambda_3 * vfc.y;
+
+                        // std::cout << "lambda: " << lambda_1 << ", " << lambda_2 << ", " << lambda_3 << std::endl;
+                        // std::cout << x_face << " == " << xf << std::endl;
+                        // std::cout << y_face << " == " << yf << std::endl;
+
+                        double z_min = std::min(std::min(vfa.z, vfb.z), vfc.z);
+                        double z_max = std::max(std::max(vfa.z, vfb.z), vfc.z);
+                        if (z_face < z_min || z_max < z_face) {
+                            // std::cout << z_min << " < " << z_face << " < " << z_max << std::endl;
+                            // throw std::runtime_error("bug");
+                        }
 
                         if (z_face < 0.0 || z_face > 1.0) {
+                            // std::cout << "z_face outside: " << z_face << std::endl;
                             continue;
                         }
                         // zは小さい方が手前
-                        float current_min_z = depth_map(yi, xi);
+                        double current_min_z = depth_map(yi, xi);
+                        // std::cout << current_min_z << " < " << z_face << std::endl;
                         if (z_face < current_min_z) {
                             // 現在の面の方が前面の場合
                             depth_map(yi, xi) = z_face;
