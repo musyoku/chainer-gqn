4caa58328b8a473c15b493cdd81b05963e33c278
musyoku <musyoku@users.noreply.github.com>
2018-06-28 02:21:38 +0900

    OpenGL 4.5

--- three/core/renderer/opengl/vao.cpp
+++ three/core/renderer/opengl/vao.cpp
@@ -46,14 +46,9 @@ namespace renderer {
 
             for (int object_index = 0; object_index < num_objects; object_index++) {
                 GLuint vao = _vao[object_index];
-                // glBindVertexArray(vao);
-
                 auto& object = scene->_objects[object_index];
                 int num_faces = object->_num_faces;
                 int num_vertices = object->_num_vertices;
-
-                // glBindBuffer(GL_ARRAY_BUFFER, _vbo_vertices[object_index]);
-                // glBufferData(GL_ARRAY_BUFFER, 3 * num_faces * sizeof(glm::vec3f), object->_face_vertices.get(), GL_STATIC_DRAW);
 
                 glNamedBufferData(_vbo_vertices[object_index], 3 * num_faces * sizeof(glm::vec3f), object->_face_vertices.get(), GL_STATIC_DRAW);
                 glEnableVertexArrayAttrib(vao, _attribute_position);
@@ -61,31 +56,24 @@ namespace renderer {
                 glVertexArrayAttribBinding(vao, _attribute_position, 0);
                 glVertexArrayVertexBuffer(vao, 0, _vbo_vertices[object_index], 0, sizeof(glm::vec3f));
 
-                // glVertexAttribPointer(_attribute_position, 3, GL_FLOAT, GL_FALSE, 0, 0);
-                // glEnableVertexAttribArray(_attribute_position);
-
                 glNamedBufferData(_vbo_normal_vectors[object_index], 3 * num_faces * sizeof(glm::vec3f), object->_face_normal_vectors.get(), GL_STATIC_DRAW);
                 glEnableVertexArrayAttrib(vao, _attribute_face_normal_vector);
                 glVertexArrayAttribFormat(vao, _attribute_face_normal_vector, 3, GL_FLOAT, GL_FALSE, 0);
                 glVertexArrayAttribBinding(vao, _attribute_face_normal_vector, 1);
                 glVertexArrayVertexBuffer(vao, 1, _vbo_normal_vectors[object_index], 0, sizeof(glm::vec3f));
 
-                glNamedBufferData(_vbo_vertex_normal_vectors[object_index], 3 * num_faces * sizeof(glm::vec3f), object->_face_normal_vectors.get(), GL_STATIC_DRAW);
+                glNamedBufferData(_vbo_vertex_normal_vectors[object_index], 3 * num_faces * sizeof(glm::vec3f), object->_face_vertex_normal_vectors.get(), GL_STATIC_DRAW);
                 glEnableVertexArrayAttrib(vao, _attribute_vertex_normal_vector);
                 glVertexArrayAttribFormat(vao, _attribute_vertex_normal_vector, 3, GL_FLOAT, GL_FALSE, 0);
                 glVertexArrayAttribBinding(vao, _attribute_vertex_normal_vector, 2);
                 glVertexArrayVertexBuffer(vao, 2, _vbo_vertex_normal_vectors[object_index], 0, sizeof(glm::vec3f));
 
-                glNamedBufferData(_vbo_vertex_colors[object_index], 3 * num_faces * sizeof(glm::vec3f), object->_face_normal_vectors.get(), GL_STATIC_DRAW);
+                glNamedBufferData(_vbo_vertex_colors[object_index], 3 * num_faces * sizeof(glm::vec4f), object->_face_vertex_colors.get(), GL_STATIC_DRAW);
                 glEnableVertexArrayAttrib(vao, _attribute_vertex_color);
                 glVertexArrayAttribFormat(vao, _attribute_vertex_color, 3, GL_FLOAT, GL_FALSE, 0);
                 glVertexArrayAttribBinding(vao, _attribute_vertex_color, 3);
-                glVertexArrayVertexBuffer(vao, 3, _vbo_vertex_colors[object_index], 0, sizeof(glm::vec3f));
-
-                // glBindVertexArray(0);
+                glVertexArrayVertexBuffer(vao, 3, _vbo_vertex_colors[object_index], 0, sizeof(glm::vec4f));
             }
-
-            // glBindVertexArray(0);
         }
         void VertexArrayObject::bind_object(int object_index)
         {
