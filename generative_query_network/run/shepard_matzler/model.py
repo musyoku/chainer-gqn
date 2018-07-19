753be9623e966bc989aac830f3fa841a4b585482
musyoku <musyoku@users.noreply.github.com>
2018-07-19 13:43:54 +0900

    Support shepard matzler

---
+++ generative_query_network/run/shepard_matzler/model.py
@@ -0,0 +1,223 @@
+import os
+import sys
+import chainer
+import uuid
+from chainer.serializers import load_hdf5, save_hdf5
+
+sys.path.append(os.path.join("..", ".."))
+import gqn
+
+from hyper_parameters import HyperParameters
+
+
+class Model():
+    def __init__(self, hyperparams: HyperParameters, hdf5_path=None):
+        assert isinstance(hyperparams, HyperParameters)
+        self.generation_steps = hyperparams.generator_generation_steps
+        self.hyperparams = hyperparams
+
+        self.generation_cores, self.generation_prior, self.generation_observation, self.generation_params = self.build_generation_network(
+            generation_steps=self.generation_steps,
+            channels_chz=hyperparams.channels_chz,
+            channels_u=hyperparams.generator_u_channels)
+
+        self.inference_cores, self.inference_posterior, self.inference_downsampler, self.inference_params = self.build_inference_network(
+            generation_steps=self.generation_steps,
+            channels_chz=hyperparams.channels_chz,
+            channels_map_x=hyperparams.inference_channels_map_x)
+
+        self.representation_network, self.representation_params = self.build_representation_network(
+            architecture=hyperparams.representation_architecture,
+            channels_r=hyperparams.channels_r)
+
+        if hdf5_path:
+            try:
+                load_hdf5(
+                    os.path.join(hdf5_path, "generation.hdf5"),
+                    self.generation_params)
+                load_hdf5(
+                    os.path.join(hdf5_path, "inference.hdf5"),
+                    self.inference_params)
+                load_hdf5(
+                    os.path.join(hdf5_path, "representation.hdf5"),
+                    self.representation_params)
+            except:
+                pass
+
+        self.parameters = chainer.Chain()
+        with self.parameters.init_scope():
+            self.parameters.generation = self.generation_params
+            self.parameters.inference = self.inference_params
+            self.parameters.representation = self.representation_params
+
+    def build_generation_network(self, generation_steps, channels_chz,
+                                 channels_u):
+        generation_parameters = chainer.Chain()
+        cores = []
+        with generation_parameters.init_scope():
+            # LSTM core
+            for t in range(generation_steps):
+                params = gqn.nn.chainer.generator.CoreParameters(
+                    channels_chz=channels_chz, channels_u=channels_u)
+                core = gqn.nn.chainer.generator.CoreNetwork(params=params)
+                cores.append(core)
+                setattr(generation_parameters, "core_%d" % t, params)
+
+            # z prior sampler
+            params = gqn.nn.chainer.generator.PriorParameters(
+                channels_z=channels_chz)
+            prior = gqn.nn.chainer.generator.PriorNetwork(params)
+            generation_parameters.prior = params
+
+            # observation sampler
+            params = gqn.nn.chainer.generator.ObservationParameters()
+            observation = gqn.nn.chainer.generator.ObservationNetwork(params)
+            generation_parameters.observation = params
+
+        return cores, prior, observation, generation_parameters
+
+    def build_inference_network(self, generation_steps, channels_chz, channels_map_x):
+        inference_parameters = chainer.Chain()
+        cores = []
+        with inference_parameters.init_scope():
+            # LSTM core
+            for t in range(generation_steps):
+                params = gqn.nn.chainer.inference.CoreParameters(
+                    channels_chz=channels_chz)
+                core = gqn.nn.chainer.inference.CoreNetwork(params=params)
+                cores.append(core)
+                setattr(inference_parameters, "core_%d" % t, params)
+
+            # z posterior sampler
+            params = gqn.nn.chainer.inference.PosteriorParameters(
+                channels_z=channels_chz)
+            posterior = gqn.nn.chainer.inference.PosteriorNetwork(params)
+            inference_parameters.posterior = params
+
+            # x downsampler
+            params = gqn.nn.chainer.inference.DownsamplerParameters(
+                channels=channels_map_x)
+            downsampler = gqn.nn.chainer.inference.Downsampler(params)
+            inference_parameters.downsampler = params
+
+        return cores, posterior, downsampler, inference_parameters
+
+    def build_representation_network(self, architecture, channels_r):
+        if architecture == "tower":
+            params = gqn.nn.chainer.representation.TowerParameters(
+                channels_r=channels_r)
+            network = gqn.nn.chainer.representation.TowerNetwork(
+                params=params)
+            return network, params
+
+        raise NotImplementedError
+
+    def to_gpu(self):
+        self.parameters.to_gpu()
+
+    def cleargrads(self):
+        self.parameters.cleargrads()
+
+    def serialize(self, path):
+        self.serialize_parameter(path, "generation.hdf5",
+                                 self.generation_params)
+        self.serialize_parameter(path, "inference.hdf5", self.inference_params)
+        self.serialize_parameter(path, "representation.hdf5",
+                                 self.representation_params)
+
+    def serialize_parameter(self, path, filename, params):
+        tmp_filename = str(uuid.uuid4())
+        save_hdf5(os.path.join(path, tmp_filename), params)
+        os.rename(
+            os.path.join(path, tmp_filename), os.path.join(path, filename))
+
+    def generate_initial_state(self, batch_size, xp):
+        h0_g = xp.zeros(
+            (
+                batch_size,
+                self.hyperparams.channels_chz,
+            ) + self.hyperparams.chrz_size,
+            dtype="float32")
+        c0_g = xp.zeros(
+            (
+                batch_size,
+                self.hyperparams.channels_chz,
+            ) + self.hyperparams.chrz_size,
+            dtype="float32")
+        u0 = xp.zeros(
+            (
+                batch_size,
+                self.hyperparams.generator_u_channels,
+            ) + self.hyperparams.image_size,
+            dtype="float32")
+        h0_e = xp.zeros(
+            (
+                batch_size,
+                self.hyperparams.channels_chz,
+            ) + self.hyperparams.chrz_size,
+            dtype="float32")
+        c0_e = xp.zeros(
+            (
+                batch_size,
+                self.hyperparams.channels_chz,
+            ) + self.hyperparams.chrz_size,
+            dtype="float32")
+        return h0_g, c0_g, u0, h0_e, c0_e
+
+    def get_generation_core(self, l):
+        return self.generation_cores[l]
+
+    def get_inference_core(self, l):
+        return self.inference_cores[l]
+
+    def generate_image(self, query_viewpoints, r, xp):
+        batch_size = query_viewpoints.shape[0]
+        h0_g, c0_g, u0, _, _ = self.generate_initial_state(batch_size, xp)
+        hl_g = h0_g
+        cl_g = c0_g
+        ul_g = u0
+        for l in range(self.generation_steps):
+            core = self.get_generation_core(l)
+            zg_l = self.generation_prior.sample_z(hl_g)
+            next_h_g, next_c_g, next_u_g = core.forward_onestep(
+                hl_g, cl_g, ul_g, zg_l, query_viewpoints, r)
+
+            hl_g = next_h_g
+            cl_g = next_c_g
+            ul_g = next_u_g
+
+        x = self.generation_observation.compute_mean_x(ul_g)
+        return x.data
+
+
+    def reconstruct_image(self, query_images, query_viewpoints, r, xp):
+        batch_size = query_viewpoints.shape[0]
+        h0_g, c0_g, u0, h0_e, c0_e = self.generate_initial_state(batch_size, xp)
+
+        hl_e = h0_e
+        cl_e = c0_e
+        hl_g = h0_g
+        cl_g = c0_g
+        ul_e = u0
+        for l in range(self.generation_steps):
+            inference_core = self.get_inference_core(l)
+            generation_core = self.get_generation_core(l)
+
+            xq = self.inference_downsampler.downsample(query_images)
+
+            he_next, ce_next = inference_core.forward_onestep(
+                hl_g, hl_e, cl_e, xq, query_viewpoints, r)
+
+            ze_l = self.inference_posterior.sample_z(hl_e)
+
+            hg_next, cg_next, ue_next = generation_core.forward_onestep(
+                hl_g, cl_g, ul_e, ze_l, query_viewpoints, r)
+
+            hl_g = hg_next
+            cl_g = cg_next
+            ul_e = ue_next
+            hl_e = he_next
+            cl_e = ce_next
+
+        x = self.generation_observation.compute_mean_x(ul_e)
+        return x.data
---
+++ generative_query_network/run/shepard_matzler/optimizer.py
@@ -0,0 +1,46 @@
+import math
+from chainer import optimizers
+from chainer.optimizer_hooks import GradientClipping
+
+
+class Optimizer:
+    def __init__(
+            self,
+            model_parameters,
+            # Learning rate at training step s with annealing
+            mu_i=5.0 * 1e-4,
+            mu_f=5.0 * 1e-5,
+            n=1.6 * 1e6,
+            # Learning rate as used by the Adam algorithm
+            beta_1=0.9,
+            beta_2=0.99,
+            # Adam regularisation parameter
+            eps=1e-8):
+        self.mu_i = mu_i
+        self.mu_f = mu_f
+        self.n = n
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+        self.eps = eps
+
+        lr = self.mu_s(0)
+        self.optimizer = optimizers.Adam(
+            lr, beta1=beta_1, beta2=beta_2, eps=eps)
+        self.optimizer.setup(model_parameters)
+
+    @property
+    def learning_rate(self):
+        return self.optimizer.alpha
+
+    def mu_s(self, training_step):
+        return max(
+            self.mu_f +
+            (self.mu_i - self.mu_f) * (1.0 - training_step / self.n),
+            self.mu_f)
+
+    def anneal_learning_rate(self, training_step):
+        self.optimizer.hyperparam.alpha = self.mu_s(training_step)
+
+    def update(self, training_step):
+        self.optimizer.update()
+        self.anneal_learning_rate(training_step)
